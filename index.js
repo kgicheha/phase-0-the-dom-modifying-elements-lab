0dc4cd6311e1c2281d4ab370c1b665fe5accd28d
Kevin Gicheha <gichehakevin@gmail.com>
2022-06-27 15:02:57 -0700

    commit tree 6f6c79a2a641fd08a378164e3f83f474e92e4d54

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your code here!
+
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1')
+
+newHeader.id = "victory"
+newHeader.textContent ='YOUR-NAME is the champion'
+newHeader.className = "victory"
+
+document.body.append(newHeader)
+
+
+const table = document.querySelector("#table")
+const item = document.createElement("ul")
+
+item.id = "newItem"
+
+//add detail to item
+item.textContent = "item1"
+
+//appen item to table
+table.append(item)
